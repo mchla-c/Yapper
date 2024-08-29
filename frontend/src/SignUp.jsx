bce36921156d6d7412ad128910b8cef9ac6bedf0
mchla-c <mcastro01204@gmail.com>
2024-08-28 22:23:48 -0700

    modified sign up page and added logo

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -15,6 +15,10 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useState } from 'react';
 import axios from 'axios'
 import { theme } from './components/palette';
+import logo from './assets/logo.png'; 
+import { cardStyle } from './components/styles';
+import { Card, CardContent } from '@mui/material';
+
 
 const defaultTheme = createTheme();
 
@@ -35,76 +39,102 @@ function SignUp() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="name"
-                  label="Full Name"
-                  name="name"
-                  autoComplete="family-name"
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link to='/signin' variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
+      <Container component="main">
+      <CssBaseline />
+        <Grid container spacing={2} sx={{ minHeight: '90vh', alignItems: 'center', justifyContent: 'center' }}>
+        <Card sx={cardStyle}>
+        <CardContent sx={{ display: 'flex', flexDirection: 'row', width: '100%', height: '100%' }}>
+          {/* Left Side: Logo Section */}
+          <Grid item xs={12} md={7} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', bgcolor: 'primary.main' }}>
+            {/* Add your logo image here */}
+            <Box
+              component="img"
+              sx={{
+                maxHeight: '100%',
+                maxWidth: '100%',
+                objectFit: 'fill',
+              }}
+              alt="Logo"
+              src={logo} // Update the path to your logo image
+            />
+          </Grid>
+
+          {/* Right Side: Sign-Up Form */}
+          <Grid item xs={12} md={5} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+            <Box sx={{ mx: 4 }}>
+              {/* <Card sx={cardStyle}>
+                <CardContent> */}
+                  <Typography variant="h3" mb={3} fontWeight={'bold'}>
+                    See who's Yapping
+                  </Typography>
+                  <Typography variant="h5" fontWeight={'bold'}>
+                    Ready to yap?
+                  </Typography>
+                  <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          id="name"
+                          label="Full Name"
+                          name="name"
+                          autoComplete="family-name"
+                          onChange={(e) => setName(e.target.value)}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          id="email"
+                          label="Email Address"
+                          name="email"
+                          autoComplete="email"
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          name="password"
+                          label="Password"
+                          type="password"
+                          id="password"
+                          autoComplete="new-password"
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </Grid>
+                    </Grid>
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2, 
+                        color: 'white', 
+                        borderRadius: '17px', 
+                        boxShadow: 2,
+                        padding: 1.2, }}
+                    >
+                      Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                      <Grid item>
+                        <Link to='/signin' variant="body2">
+                          Already have an account? Sign in
+                        </Link>
+                      </Grid>
+                    </Grid>
+                  </Box>
+                 {/* </CardContent>
+              </Card> */}
+            </Box>
+
+          </Grid>
+          </CardContent>
+              </Card>
+        </Grid>
       </Container>
    </ThemeProvider>
   );
