62d8b7e510188a57cb2e1d7831684e29a0ef6036
mchla-c <mcastro01204@gmail.com>
2024-08-31 22:28:41 -0700

    added user routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
-import authRoutes from "./routes/auth.routes.js"
 
 import dotenv from 'dotenv'
 import express from "express"
-import connectMongoDB from "./db/connectMongoDB.js"
 import cookieParser from "cookie-parser"
+import {v2 as cloudinary} from "cloudinary"
+
+import authRoutes from "./routes/auth.routes.js"
+import connectMongoDB from "./db/connectMongoDB.js"
+import userRoutes from "./routes/user.routes.js"
 
 dotenv.config()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 // const express = require("express")
 // const mongoose = require('mongoose')
@@ -27,6 +36,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes)
 
 // app.post("/signin", (req, res) => {
 //     const {email, password} = req.body
