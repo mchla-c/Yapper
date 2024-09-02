01c559c8066987e2c1894ba7c269042df28fe053
Fahim Zaman <fahim.zaman110@gmail.com>
2024-09-01 19:57:10 -0700

    added notif routes and controller

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 dotenv.config();
 
@@ -38,6 +39,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 // app.post("/signin", (req, res) => {
 //     const {email, password} = req.body
