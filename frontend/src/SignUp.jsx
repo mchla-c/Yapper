99eeae1798c244f5800226dea7cf925e8813ea97
Fahim Zaman <fahim.zaman110@gmail.com>
2024-09-09 00:53:04 -0700

    img for signup

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -1,156 +1,164 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import {Link, useNavigate} from 'react-router-dom'
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useState } from 'react';
-import axios from 'axios'
-import { theme } from './components/palette';
-import logo from './assets/logo.png'; 
-import { cardStyle } from './components/styles';
-import { Card, CardContent, CardMedia } from '@mui/material';
-import { AuthCard } from './components/authcard';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import toast from 'react-hot-toast';
-
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { Link, useNavigate } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import axios from "axios";
+import { theme } from "./components/palette";
+import logo from "./assets/logo.png";
+import { cardStyle } from "./components/styles";
+import { Card, CardContent, CardMedia } from "@mui/material";
+import { AuthCard } from "./components/authcard";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const defaultTheme = createTheme();
 
 function SignUp() {
-    const [formData, setFormData] = useState({
-      email: "",
-      username: "",
-      fullName: "",
-      password: "",
-    });
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    fullName: "",
+    password: "",
+  });
 
-    const {mutate, isError, isPending, error} = useMutation({
-      mutationFn: async({fullName, username, email, password}) => {
-        try {
-          const res = await fetch("/api/auth/signup", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({fullName, username, email, password})
-          })
+  const { mutate, isError, isPending, error } = useMutation({
+    mutationFn: async ({ fullName, username, email, password }) => {
+      try {
+        const res = await fetch("/api/auth/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ fullName, username, email, password }),
+        });
 
-          
-          const data = await res.json()
-          if (!res.ok) throw new Error(data.error || "Failed to create account")
-          console.log(data)
-          return data
-        } catch (error) {
-          console.log(error)
-          throw error
-        }
-      },
-
-      onSuccess: () => {
-        toast.success("Account created successfully")
-        queryClient.invalidateQueries({queryKey: ["authUser"]})
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.error || "Failed to create account");
+        console.log(data);
+        return data;
+      } catch (error) {
+        console.log(error);
+        throw error;
       }
-    })
-    
+    },
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        mutate(formData)
-    }
+    onSuccess: () => {
+      toast.success("Account created successfully");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+  });
 
-    const handleInputChange = (e) => {
-      setFormData({...formData, [e.target.name]: e.target.value})
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    mutate(formData);
+  };
 
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   return (
     <ThemeProvider theme={theme}>
-        <AuthCard logosrc={logo} title="See who's Yapping" subtitle="Ready to yap?">
-                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3, mr: 3 }}>
-                    <Grid container spacing={2}>
-                      <Grid item xs={12}>
-                        <TextField
-                          required
-                          fullWidth
-                          id="fullName"
-                          label="Full Name"
-                          name="fullName"
-                          autoComplete="family-name"
-                          value={formData.fullName}
-                          onChange={handleInputChange}
-                        />
-                      </Grid>
-                      <Grid item xs={12}>
-                        <TextField
-                          required
-                          fullWidth
-                          id="username"
-                          label="Username"
-                          name="username"
-                          autoComplete="username"
-                          value={formData.username}
-                          onChange={handleInputChange}
-                        />
-                    </Grid>
-                     <Grid item xs={12}>
-                        <TextField
-                          required
-                          fullWidth
-                          id="email"
-                          label="Email Address"
-                          name="email"
-                          autoComplete="email"
-                          value={formData.email}
-                          onChange={handleInputChange}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="new-password"
-                        value={formData.password}
-                        onChange={handleInputChange}
-                      />
-                    </Grid>
-                  </Grid>
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2, 
-                    color: 'white', 
-                    borderRadius: '17px', 
-                    boxShadow: 2,
-                    padding: 1.2, }}
-                  >
-                    {isPending ? "Loading..." : "Sign Up"}
-                  </Button>
-                  {isError && <Typography color={'red'}>{error.message}</Typography>}
-                  <Grid container justifyContent="flex-end" sx={{mb: 3}}>
-                    <Grid item>
-                      <Link to='/signin' variant="body2">
-                        Already have an account? Sign in
-                      </Link>
-                  </Grid>
-                </Grid>
-              </Box>
-        </AuthCard>
-   </ThemeProvider>
+      <AuthCard
+        logosrc="https://res.cloudinary.com/daistfxjs/image/upload/v1725504615/hylygyewelxii1mfbv5w.png"
+        title="See who's Yapping"
+        subtitle="Ready to yap?"
+      >
+        <Box
+          component="form"
+          noValidate
+          onSubmit={handleSubmit}
+          sx={{ mt: 3, mr: 3 }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="fullName"
+                label="Full Name"
+                name="fullName"
+                autoComplete="family-name"
+                value={formData.fullName}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+                value={formData.username}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              color: "white",
+              borderRadius: "17px",
+              boxShadow: 2,
+              padding: 1.2,
+            }}
+          >
+            {isPending ? "Loading..." : "Sign Up"}
+          </Button>
+          {isError && <Typography color={"red"}>{error.message}</Typography>}
+          <Grid container justifyContent="flex-end" sx={{ mb: 3 }}>
+            <Grid item>
+              <Link to="/signin" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </AuthCard>
+    </ThemeProvider>
   );
 }
 
-export default SignUp
+export default SignUp;
