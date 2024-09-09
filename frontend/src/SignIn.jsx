f0a253628912184fb80dc509d85d8549138f928a
Fahim Zaman <fahim.zaman110@gmail.com>
2024-09-09 00:37:14 -0700

    logo update

--- frontend/src/SignIn.jsx
+++ frontend/src/SignIn.jsx
@@ -1,75 +1,87 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import {Link, useNavigate} from 'react-router-dom'
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useState } from 'react';
-import axios from 'axios'
-import Palette, { theme } from './components/palette';
-import { AuthCard } from './components/authcard';
-import logo from './assets/logo.png'
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { Link, useNavigate } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import axios from "axios";
+import Palette, { theme } from "./components/palette";
+import { AuthCard } from "./components/authcard";
+import Logo from "./assets/logo.png";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 //const defaultTheme = createTheme();
 
 export default function SignIn() {
-    const [formData, setFormData] = useState({
-      username: "",
-      password: "",
-    });
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
 
-    const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
-    const {mutate: signin, isPending, isError, error} = useMutation({
-      mutationFn: async({username, password}) => {
-        try {
-          const res = await fetch("/api/auth/signin", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({username, password})
-          })
+  const {
+    mutate: signin,
+    isPending,
+    isError,
+    error,
+  } = useMutation({
+    mutationFn: async ({ username, password }) => {
+      try {
+        const res = await fetch("/api/auth/signin", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        });
 
-          const data = await res.json()
+        const data = await res.json();
 
-          if (!res.ok) {
-            throw new Error(data.error || "Something went wrong")
-          }
-
-          
-        } catch (error) {
-          throw new Error(error)
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
         }
-      },
-      onSuccess: () => {
-        // toast.success("Login Successful")
-        queryClient.invalidateQueries({queryKey: ["authUser"]})
+      } catch (error) {
+        throw new Error(error);
       }
-    })
+    },
+    onSuccess: () => {
+      // toast.success("Login Successful")
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+  });
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        signin(formData)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signin(formData);
+  };
 
-    const handleInputChange = (e) => {
-      setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   return (
     <ThemeProvider theme={theme}>
-        <AuthCard logosrc={logo} title="Welcome back to Yapper!" subtitle="Sign in">
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 3, mr: 3 }}>
+      <AuthCard
+        logosrc={Logo}
+        title="Welcome back to Yapper!"
+        subtitle="Sign in"
+      >
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          sx={{ mt: 3, mr: 3 }}
+        >
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
@@ -82,55 +94,58 @@ export default function SignIn() {
                 autoComplete="username"
                 autoFocus
                 onChange={handleInputChange}
-							  value={formData.username}
+                value={formData.username}
               />
             </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  onChange={handleInputChange}
-							    value={formData.password}
-                />
-              </Grid>
+            <Grid item xs={12}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={handleInputChange}
+                value={formData.password}
+              />
             </Grid>
-            {/* <FormControlLabel
+          </Grid>
+          {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             /> */}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2, 
-              color: 'white', 
-              borderRadius: '17px', 
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              color: "white",
+              borderRadius: "17px",
               boxShadow: 2,
-              padding: 1.2, }}
-            >
-              {isPending ? "Loading..." : "Sign In"}
-            </Button>
-            {isError && <Typography color={'red'}>{error.message}</Typography>}
-            <Grid container justifyContent="flex-end" sx={{mb: 3}}>
-              {/* <Grid item xs>
+              padding: 1.2,
+            }}
+          >
+            {isPending ? "Loading..." : "Sign In"}
+          </Button>
+          {isError && <Typography color={"red"}>{error.message}</Typography>}
+          <Grid container justifyContent="flex-end" sx={{ mb: 3 }}>
+            {/* <Grid item xs>
                 <Link href="#" variant="body2">
                   Forgot password?
                 </Link>
               </Grid> */}
-              <Grid item>
-                <Link to='/signup' variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+            <Grid item>
+              <Link to="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
             </Grid>
-          </Box>
-        </AuthCard>
+          </Grid>
+        </Box>
+      </AuthCard>
     </ThemeProvider>
   );
 }
