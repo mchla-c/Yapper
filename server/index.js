96f8389eeee8553e044b0a3d42eb3c37806e5e1d
mchla-c <mcastro01204@gmail.com>
2024-09-08 23:04:59 -0700

    prepare for deployment

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -20,18 +21,8 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// const express = require("express")
-// const mongoose = require('mongoose')
-// const cors = require("cors")
-// const UsersModel = require('./models/users')
-
 const PORT = process.env.PORT || 3001;
-// app.use(express.json())
-// app.use(cors())
-
-// const uri = process.env.MONGO_URI
-
-// mongoose.connect(uri)
+const __dirname = path.resolve()
 
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true }));
@@ -44,27 +35,13 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 app.use("/api/messages", messageRoutes)
 
-// app.post("/signin", (req, res) => {
-//     const {email, password} = req.body
-//     UsersModel.findOne({email: email})
-//     .then(user => {
-//         if(user) {
-//             if(user.password === password) {
-//                 res.json("Success")
-//             } else {
-//                 res.json("Password is incorrect")
-//             }
-//         } else {
-//             res.json("No record existed")
-//         }
-//     })
-// })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-// app.post('/signup', (req, res) => {
-//     UsersModel.create(req.body)
-//     .then(users => res.json(users))
-//     .catch(err => res.json(err))
-// })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
 
 server.listen(PORT, () => {
   console.log("Server is running");
